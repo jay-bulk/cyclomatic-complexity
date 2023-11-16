dec54de0260f6efccdbced7ea8c02abe6a1f0474
jay-bulk <jay.bulk@proton.me>
2023-11-15 23:59:38 -0700

    fix: main.c

--- main.c
+++ main.c
@@ -1,68 +1,74 @@
 #include <stdio.h>
+#include <string.h>
 
-void CheckBoeingString(char *input, int stg, char *error_switch);
-void CheckEndString(char *input, int stg);
+void CheckBoeingString(char *aircraft, int stg, char *error_switch);
+void CheckEndString(char *aircraft, int stg);
 
 int main() {
   int stg = 0;
-  char *input = "Default String";
-  scanf(input);
-  char *error_switch = "no";
+  char* aircraft;
+  char error_switch[3] = "no";
 
-  CheckBoeingString(input, stg, error_switch);
+  CheckBoeingString(aircraft, stg, error_switch);
 
   return 0;
 }
 
-void CheckBoeingString(char *input, int stg, char *error_switch) {
-  if (input[stg] == 'B') {
+void CheckBoeingString() {
+  char error_switch[3];
+  if (aircraft[stg] == 'B') {
     ++stg;
   } else {
     error_switch = "yes";
-    CheckEndString(input, stg);
+    CheckEndString(aircraft, stg);
   }
 
-  if (input[stg] == '-') {
+  if (&aircraft[stg] == '-') {
     stg++;
   } else {
     error_switch = "yes";
-    CheckEndString(input, stg);
+    CheckEndString(aircraft, stg);
   }
 
-  if (input[stg] == '7') {
+  if (&aircraft[stg] == '7') {
     stg++;
   } else {
     error_switch = "yes";
-    CheckEndString(input, stg);
+    CheckEndString(aircraft, stg);
   }
 
-  if (input[stg] == '3' || input[stg] == '4' || input[stg] == '5' ||
-      input[stg] == '6' || input[stg] == '7' || input[stg] == '8') {
+  if (aircraft[stg] == '3' || aircraft[stg] == '4' || aircraft[stg] == '5' ||
+      aircraft[stg] == '6' || aircraft[stg] == '7' || aircraft[stg] == '8') {
     stg++;
   } else {
     error_switch = "yes";
-    CheckEndString(input, stg);
+    CheckEndString(aircraft, stg);
   }
 
-  if (input[stg] == '7') {
+  if (aircraft[stg] == '7') {
     stg++;
   } else {
     error_switch = "yes";
-    CheckEndString(input, stg);
+    CheckEndString(aircraft, stg);
   }
 }
 
-void CheckEndString(char *input, int stg) {
+void CheckEndString() {
   char *error_switch = "no";
-  if (input[stg] == ',') {
+  if (aircraft[stg] == ',') {
     stg++;
-    CheckBoeingString(input, stg, error_switch);
-  } else if (input[stg] != ' ') {
-    // The character is not blank
+    CheckBoeingString(&aircraft, stg, error_switch);
+  } else if (aircraft[stg] != ' ') {
+    /*  
+    * This doesn't make sense... the PDL said 'not = " " comment "the character is blank" '. 
+    * Is the code meant to check if it is blank?
+    */
+    printf("The character %c is blank", aircraft[stg]);
     stg++;
-    CheckBoeingString(input, stg, error_switch);
+    CheckBoeingString(aircraft, stg, error_switch);
   } else {
     // Stop
+    printf("Squawk Received: I am a %s", aircraft);
     return; // Add the return statement to exit the function
   }
 }
